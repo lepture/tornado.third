25a80889921d87cae29ad4894a5cb0783c1580cd
Guan Hao <raptium@gmail.com>
2011-12-20 22:02:41 +0800

    Fixed the `statuses/upload` request by adding a multipart/form-data builder.
    This commit is not tested yet.

--- weibo.py
+++ weibo.py
@@ -3,6 +3,8 @@
 
 import logging
 import urllib
+import mimetools
+import itertools
 
 from tornado import escape
 from tornado.httputil import url_concat
@@ -15,7 +17,7 @@ class WeiboMixin(OAuth2Mixin):
     _OAUTH_NO_CALLBACKS = False
 
     def get_authenticated_user(self, redirect_uri, client_id, client_secret,
-                              code, callback, extra_fields=None):
+                               code, callback, extra_fields=None):
         """Handles the login for the Weibo user, returning a user object.
 
            Example usage::
@@ -46,7 +48,7 @@ class WeiboMixin(OAuth2Mixin):
             "client_id": client_id,
             "client_secret": client_secret,
             "extra_params": {"grant_type": "authorization_code"},
-        }
+            }
         post_args = args.copy()
         post_args.update({"grant_type": "authorization_code"})
 
@@ -54,19 +56,19 @@ class WeiboMixin(OAuth2Mixin):
         if extra_fields: fields.update(extra_fields)
 
         http.fetch(
-            self._oauth_request_token_url(**args), 
+            self._oauth_request_token_url(**args),
             method="POST",
             body=urllib.urlencode(post_args),
-            callback=self.async_callback(self._on_access_token, redirect_uri, 
-                        client_id, client_secret, callback, fields))
+            callback=self.async_callback(self._on_access_token, redirect_uri,
+                client_id, client_secret, callback, fields))
 
     def _on_access_token(self, redirect_uri, client_id, client_secret,
-                        callback, fields, response):
+                         callback, fields, response):
         if response.error:
             logging.warning('Weibo auth error: %s' % str(response))
             callback(None)
             return
-        
+
         json = escape.json_decode(response.body)
         session = {
             "access_token": json["access_token"],
@@ -77,7 +79,7 @@ class WeiboMixin(OAuth2Mixin):
             path="/account/get_uid.json",
             callback=self.async_callback(
                 self._on_get_user_id, callback, session, fields),
-                access_token=session["access_token"],
+            access_token=session["access_token"],
         )
 
 
@@ -85,32 +87,32 @@ class WeiboMixin(OAuth2Mixin):
         if user is None:
             callback(None)
             return
-        
+
         fields = ["id", "screen_name", "profile_image_url"]
         uid = user["uid"]
         self.weibo_request(
             path="/users/show.json",
             callback=self.async_callback(
-            self._on_get_user_info, callback, session, fields),
+                self._on_get_user_info, callback, session, fields),
             access_token=session["access_token"],
             uid=uid
         )
-    
+
     def _on_get_user_info(self, callback, session, fields, user):
         if user is None:
             callback(None)
             return
-        
+
         fieldmap = {}
         for field in fields:
             fieldmap[field] = user.get(field)
 
         fieldmap.update({"access_token": session["access_token"], "session_expires": session.get("expires_in")})
         callback(fieldmap)
-        
+
 
     def weibo_request(self, path, callback, access_token=None,
-                           post_args=None, **args):
+                      post_args=None, **args):
         """Fetches the given relative API path
 
         If the request is a POST, post_args should be provided. Query
@@ -140,8 +142,36 @@ class WeiboMixin(OAuth2Mixin):
                         return
                     self.finish("Posted a message!")
 
+
+        To send a request to ``/statuses/upload``, the ``pic`` parameter is
+        required and it should be a dict with ``filename``, ``content`` and
+        ``mime_type`` set. Example usage::
+
+            class UploadHandler(tornado.web.RequestHandler, WeiboMixin):
+                @tornado.web.authenticated
+                @tornado.web.asynchronous
+                @tornado.gen.engine
+                def get(self):
+                    # ...
+                    f = open('foo.png', 'r') # open the image file
+                    pic = {
+                        'filename': 'foo.png',
+                        'content': f.read(),
+                        'mime_type': 'image/png'
+                    }
+                    f.close()
+                    result = yield tornado.gen.Task(self.weibo_request, 'statuses/upload',
+                        access_token=self.current_user["access_token"],
+                        status='I like this photo!',
+                        pic=pic
+                    )
+                    # do something with the result ...
         """
         url = "https://api.weibo.com/2" + path
+        if path == "/statuses/upload.json":
+            # this request should be handled differently
+            return self._weibo_upload_request(url, callback,
+                access_token, args.get("pic"), status=args.get("status"))
         all_args = {}
         if access_token:
             all_args["access_token"] = access_token
@@ -152,15 +182,101 @@ class WeiboMixin(OAuth2Mixin):
         http = httpclient.AsyncHTTPClient()
         if post_args is not None:
             http.fetch(url, method="POST", body=urllib.urlencode(post_args),
-                       callback=callback)
+                callback=callback)
         else:
             http.fetch(url, callback=callback)
 
     def _on_weibo_request(self, callback, response):
         if response.error:
             logging.warning("Error response %s fetching %s", response.error,
-                            response.request.url)
+                response.request.url)
             callback(None)
             return
         callback(escape.json_decode(response.body))
 
+
+    def _weibo_upload_request(self, url, callback,
+                              access_token, pic, status=None):
+        # build and send the multipart/form-data request
+        if pic is None:
+            raise Exception("pic is required!")
+        form = MultiPartForm()
+        form.add_file("pic", pic["filename"], pic["content"], pic["mime_type"])
+        form.add_field("status", status)
+        headers = {
+            "Content-Type": form.get_content_type()
+        }
+        args = {
+            "access_token": access_token
+        }
+        url += "?" + urllib.urlencode(args)
+        http = httpclient.AsyncHTTPClient()
+        http.fetch(url, method="POST", body=str(form),
+            callback=self.async_callback(self._on_weibo_request, callback),
+            headers=headers)
+
+
+class MultiPartForm(object):
+    """Helper class to build a multipart form
+
+    Copied from http://www.doughellmann.com/PyMOTW/urllib2/
+    """
+
+    def __init__(self):
+        self.form_fields = []
+        self.files = []
+        self.boundary = mimetools.choose_boundary()
+        return
+
+    def get_content_type(self):
+        return 'multipart/form-data; boundary=%s' % self.boundary
+
+    def add_field(self, name, value):
+        """Add a simple field to the form data."""
+        self.form_fields.append((name, value))
+        return
+
+    def add_file(self, fieldname, filename, body, mimetype):
+        """Add a file to be uploaded."""
+        self.files.append((fieldname, filename, mimetype, body))
+        return
+
+    def __str__(self):
+        """Return a string representing the form data,
+        including attached files.
+        """
+        # Build a list of lists, each containing "lines" of the
+        # request.  Each part is separated by a boundary string.
+        # Once the list is built, return a string where each
+        # line is separated by '\r\n'.
+        parts = []
+        part_boundary = '--' + self.boundary
+
+        # Add the form fields
+        parts.extend(
+            [part_boundary,
+             'Content-Disposition: form-data; name="%s"' % name,
+             '',
+             value,
+             ]
+            for name, value in self.form_fields
+        )
+
+        # Add the files to upload
+        parts.extend(
+            [part_boundary,
+             'Content-Disposition: form-data; name="%s"; filename="%s"' %\
+             (field_name, filename),
+             'Content-Type: %s' % content_type,
+             '',
+             body,
+             ]
+            for field_name, filename, content_type, body in self.files
+        )
+
+        # Flatten the list and add closing boundary marker,
+        # then return CR+LF separated data
+        flattened = list(itertools.chain(*parts))
+        flattened.append('--' + self.boundary + '--')
+        flattened.append('')
+        return '\r\n'.join(flattened)
