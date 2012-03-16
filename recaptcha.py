ed0be465d56faac338d01466e65f0ba9b39f651c
lepture <lepture@me.com>
2012-03-17 02:07:45 +0800

    use options instead of settings, so that we can have less settings, pep8 validate

--- recaptcha.py
+++ recaptcha.py
@@ -1,58 +1,52 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2012, lepture.com
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#    * Redistributions in binary form must reproduce the above
+#      copyright notice, this list of conditions and the following
+#      disclaimer in the documentation and/or other materials provided
+#      with the distribution.
+#    * Neither the name of the author nor the names of its contributors
+#      may be used to endorse or promote products derived from this
+#      software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import logging
 import urllib
 
 from tornado.auth import httpclient
+from tornado.options import options
+
 
 class RecaptchaMixin(object):
-    """
-    Example usage::
-
-        class SignupHandler(BaseHandler, RecaptchaMixin):
-            def get(self):
-                if self.current_user:
-                    return self.redirect('/')
-                form = SignupForm()
-                recaptcha = self.recaptcha_render()
-                return self.render('auth/signup.html', form=form, recaptcha=recaptcha)
-
-            @asynchronous
-            def post(self):
-                form = SignupForm(self.request.arguments)
-                if form.validate():
-                    user = form.save()
-                    user.signup_ip = user.signin_ip = self.request.remote_ip
-                    self.recaptcha_validate(self.async_callback(self._on_validate, user))
-                    return
-
-                recaptcha = self.recaptcha_render()
-                html = self.render_string('auth/signup.html', form=form, recaptcha=recaptcha)
-                self.write(html)
-                self.finish()
-
-            def _on_validate(self, user, response):
-                if not response:
-                    form = SignupForm(self.request.arguments)
-                    recaptcha = self.recaptcha_render()
-                    html = self.render_string('auth/signup.html', form=form, recaptcha=recaptcha)
-                    self.write(html)
-                    self.finish()
-                    return
-                self.db.add(user)
-                self.db.commit()
-                self.set_secure_cookie('user', user.email)
-                return self.redirect('/')
-    """
     RECAPTCHA_VERIFY_URL = "http://www.google.com/recaptcha/api/verify"
 
     def recaptcha_render(self):
         token = self._recaptcha_token()
-        html = u'<div id="recaptcha_div"></div>'
-        html += u'<script type="text/javascript" src="http://www.google.com/recaptcha/api/js/recaptcha_ajax.js"></script>'
-        s = u'<script type="text/javascript">Recaptcha.create("%(key)s", "recaptcha_div", {theme: "%(theme)s",callback: Recaptcha.focus_response_field});</script>'
-        html += s % token
-        return html
+        html = ('<div id="recaptcha_div"></div><script type="text/javascript" '
+                'src="http://www.google.com/recaptcha/api/js/recaptcha_ajax.js'
+                '"></script><script type="text/javascript">Recaptcha.create'
+                '("%(key)s", "recaptcha_div", {theme: "%(theme)s",callback:'
+                'Recaptcha.focus_response_field});</script>')
+        return html % token
 
     def recaptcha_validate(self, callback):
         token = self._recaptcha_token()
@@ -82,14 +76,10 @@ class RecaptchaMixin(object):
             logging.warning("Recaptcha verify failed %s", message)
             callback(None)
 
-
     def _recaptcha_token(self):
-        self.require_setting("recaptcha_public_key", "Recaptcha Public Key")
-        self.require_setting("recaptcha_private_key", "Recaptcha Private Key")
-        self.require_setting("recaptcha_theme", "Recaptcha Theme")
         token = dict(
-            key=self.settings["recaptcha_public_key"],
-            secret=self.settings["recaptcha_private_key"],
-            theme=self.settings["recaptcha_theme"],
+            key=options.recaptcha_key,
+            secret=options.recaptcha_secret,
+            theme=options.recaptcha_theme,
         )
         return token
